a62bcc4ffa2c996bd705e75da444fd6454ce4af9
michaelvalenzuela <20mvalen@gmail.com>
2021-02-09 16:26:52 -0800

    Added Search page and functionality

---
+++ client/components/search-bar.jsx
@@ -0,0 +1,71 @@
+import React from "react";
+
+//Add more styling later
+export default class SearchBar extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      category: "",
+      location: ""
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+
+  }
+
+  handleChange(event){
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  }
+
+  handleSubmit(event){
+    event.preventDefault();
+    const req = {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+    };
+    fetch(`/api/yelp/search/${this.state.location}/${this.state.category}`, req)
+      .then(res => res.json())
+      .then(result => {
+        console.log(result);
+      });
+  }
+
+  render(){
+    const { handleChange, handleSubmit } = this;
+    return(
+      <div className="container d-flex justify-content-center">
+        <form onSubmit={handleSubmit}>
+          <label style={{ fontWeight: "bold" }} className="p-1">FIND
+            <input
+              type="text"
+              id="category"
+              name="category"
+              value={this.state.category}
+              onChange={handleChange}
+              required
+              className="m-1 ps-1"
+              placeholder="food"
+              />
+          </label>
+          <label style={{fontWeight:"bold"}} className="p-1">NEAR
+            <input
+              type="text"
+              id="location"
+              name="location"
+              value={this.state.location}
+              onChange={handleChange}
+              required
+              className="m-1 ps-1"
+              placeholder="address, state or zip"
+            />
+          </label>
+          <button className="btn"><i className="fas fa-search"></i></button>
+        </form>
+      </div>
+   );
+  }
+}
--- server/index.js
+++ server/index.js
@@ -1,8 +1,13 @@
 require('dotenv/config');
 const express = require('express');
+const ClientError = require('./client-error'); // eslint-disable-line
 const staticMiddleware = require('./static-middleware');
+const errorMiddleware = require('./error-middleware');
+const YelpApi = require('./yelp');
 
+const yelp = new YelpApi(process.env.YELP_API_KEY);
 const app = express();
+
 
 const http = require('http').createServer(app);
 const io = require('socket.io')(http, {
@@ -13,12 +18,25 @@ const io = require('socket.io')(http, {
 
 app.use(staticMiddleware);
 
+//Yelp API calls
+app.get("/api/yelp/search/:location/:category", (req, res, next) => {
+  const {location, category} = req.params;
+  if(!location || !category){
+    throw new ClientError(400, "location and category are required fields");
+  }
+  yelp.getYelpBusinesses(location, category)
+      .then(res => {
+        //Change later, using 1 for testing purposes
+        console.log("test", res[0]);
+      })
+      .catch(err => next(err));
+});
+
 // Chat room server
 // Socket being a client
 // io is the server
 const rooms = [];
 const socketIds = [];
-
 io.on('connection', socket => {
 
   socket.on('joinRoom', ({ username, room }) => {
@@ -75,6 +93,8 @@ io.on('connection', socket => {
   });
 
 });
+
+app.use(errorMiddleware);
 
 http.listen(process.env.PORT, () => {
   // eslint-disable-next-line no-console
